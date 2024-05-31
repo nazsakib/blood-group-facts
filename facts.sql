283004852ac878a0d5e0b04750891a0f0e6dc65a
nazsakib <sakibsnaz@gmail.com>
2024-05-31 16:57:15 +0600

    data inserted into table a_positive

--- facts.sql
+++ facts.sql
@@ -44,3 +44,15 @@ CREATE TABLE IF NOT EXISTS o_negative (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     facts VARCHAR(355)
 );
+
+-- inserting data into table a positive
+INSERT INTO a_positive (facts) VALUES 
+('People with A+ blood group are known for their leadership skills.');
+INSERT INTO a_positive (facts) VALUES 
+('A+ blood group individuals might be more susceptible to stress. Remember to take breaks and manage stress levels!');
+INSERT INTO a_positive (facts) VALUES 
+('A+ blood donors are always needed! Consider donating to help others.');
+INSERT INTO a_positive (facts) VALUES 
+('A+ blood group has both A and RhD antigens on its red blood cells.');
+INSERT INTO a_positive (facts) VALUES 
+('A+ blood type might offer a slight advantage in fighting certain infections due to specific immune system characteristics.');
